18ddc019e962b668cb782e64365b873ea4b70cf3
Emma-Chang <cww819.2008@163.com>
2016-12-08 14:36:04 +0800

    test
    test

---
+++ JScript.js
@@ -0,0 +1,6 @@
+﻿var http = require('http');
+http.createServer(function (request, response) {
+    response.writeHead(200, { 'Content-Type': 'text-plain' });
+    response.end('hello world');
+}).listen(8882);
+console.log('server runing at http://172.0.0.1:8882/');
